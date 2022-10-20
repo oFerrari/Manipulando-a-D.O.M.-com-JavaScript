52e298755027b8d038d868ded348b7f68fd65fe7
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-10-20 16:55:59 -0300

    Testando tag body

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,12 @@
     console.log("Cliquei");
 } */
 
-const button = document.getElementById('mode-selector')
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body');
 
+
+console.log(body)
 
 button.addEventListener('click', () => {
     console.log("Cliquei");
