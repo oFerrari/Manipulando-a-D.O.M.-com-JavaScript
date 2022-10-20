4c796311837a6ed7dc8439ee28355e349b003963
Andrei Ferrari <andrei.domingos@estudante.ifms.edu.br>
2022-10-20 16:56:49 -0300

    adicionando footer e pegando posição

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,8 @@
 
 const button = document.getElementById('mode-selector');
 const h1 = document.getElementById('page-title');
-const body = document.getElementsByTagName('body');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
 
 
 console.log(body)
